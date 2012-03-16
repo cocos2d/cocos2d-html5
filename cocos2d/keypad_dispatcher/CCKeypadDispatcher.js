32875e1a1d17de367638b4ff1896a5ba0fb81d8e
Hao Wu <wuhao@cocos2d-x.org>
2012-03-16 18:16:28 +0800

    1006 keypaddispatcher

--- cocos2d/keypad_dispatcher/CCKeypadDispatcher.js
+++ cocos2d/keypad_dispatcher/CCKeypadDispatcher.js
@@ -32,6 +32,116 @@ var cc = cc = cc || {};
 
 cc.kTypeBackClicked = 1;
 cc.kTypeMenuClicked = 2;
+cc.key = {
+    backspace:8,
+    tab:9,
+    enter:13,
+    shift:16,//should use shiftkey instead
+    ctrl:17,//should use ctrlkey
+    alt:18,//should use altkey
+    pause:19,
+    capslock:20,
+    escape:27,
+    pageup:33,
+    pagedown:34,
+    end:35,
+    home:36,
+    left:37,
+    up:38,
+    right:39,
+    down:40,
+    insert:45,
+    delete:46,
+    0:48,
+    1:49,
+    2:50,
+    3:51,
+    4:52,
+    5:53,
+    6:54,
+    7:55,
+    8:56,
+    9:57,
+    a:65,
+    b:66,
+    c:67,
+    d:68,
+    e:69,
+    f:70,
+    g:71,
+    h:72,
+    i:73,
+    j:74,
+    k:75,
+    l:76,
+    m:77,
+    n:78,
+    o:79,
+    p:80,
+    q:81,
+    r:82,
+    s:83,
+    t:84,
+    u:85,
+    v:86,
+    w:87,
+    x:88,
+    y:89,
+    z:90,
+    num0:96,
+    num1:97,
+    num2:98,
+    num3:99,
+    num4:100,
+    num5:101,
+    num6:102,
+    num7:103,
+    num8:104,
+    num9:105,
+    '*':106,
+    '+':107,
+    '-':109,
+    'numdel':110,
+    '/':111,
+    f1:112,//f1-f12 dont work on ie
+    f2:113,
+    f3:114,
+    f4:115,
+    f5:116,
+    f6:117,
+    f7:118,
+    f8:119,
+    f9:120,
+    f10:121,
+    f11:122,
+    f12:123,
+    numlock:144,
+    scrolllock:145,
+    semicolon:186,
+    ',':186,
+    equal:187,
+    '=':187,
+    ';':188,
+    comma:188,
+    dash:189,
+    '.':190,
+    period:190,
+    forwardslash:191,
+    grave:192,
+    '[':219,
+    openbracket:219,
+    ']':221,
+    closebracket:221,
+    backslash:220,
+    quote:222,
+    space:32
+};
+/*keymap usage
+to mark a keydown, do cc.keyDown[65] = true; or cc.keyMap[cc.key.a]
+to mark a keyup, do cc.keyDown[65] = false; or = null,
+to find out if a key is down, check if(cc.keyDown[65]) or if,(cc.keyDown[cc.key.space])
+if its undefined or false or null, its not pressed
+ */
 
 cc.s_KeypadDispatcher = null;
 /**
@@ -39,6 +149,7 @@ cc.s_KeypadDispatcher = null;
  @brief Dispatch the keypad message from the phone
  */
 cc.KeypadDispatcher = cc.Class.extend({
+    _keydown:[],
     /**
      @brief add delegate to concern keypad msg
      */
@@ -111,46 +222,36 @@ cc.KeypadDispatcher = cc.Class.extend({
     /**
      @brief dispatch the key pad msg
      */
-    dispatchKeypadMSG: function(nMsgType)
+    dispatchKeypadMSG: function(e, keydown)
     {
-        var pHandler;
-        var pDelegate;
-        var iter;
-
         this._m_bLocked = true;
-
-        if (this._m_pDelegates.count() > 0)
+        //update keymap
+        if(keydown && !this._keydown[e.keyCode])//if keydown and our keymap doesnt have it
         {
-            for (iter = this._m_pDelegates.begin(); iter != this_m_pDelegates.end(); ++iter)
+            this._keydown[e.keyCode] = true;
+            //execute all deletegate that registered a keyboard event
+            var keys = this._keydown;
+            this._m_pDelegates.every(function(pDelegate)
             {
-                if(!(iter)){
-                    break;
-                }
-
-                pHandler = iter;
-                pDelegate = pHandler.getDelegate();
-
-                switch (nMsgType)
-                {
-                    case kTypeBackClicked:
-                        pDelegate.keyBackClicked();
-                        break;
-                    case kTypeMenuClicked:
-                        pDelegate.keyMenuClicked();
-                        break;
-                    default:
-                        break;
-                }
-            }
+                pDelegate.keyDown(keys);
+            });
         }
-
+        else if(!keydown && this._keydown[e.keyCode])//if keyup and our keymap have that key in it
+        {
+            this._keydown.splice(e.keyCode,1);
+            var keys = this._keydown;
+            this._m_pDelegates.every(function(pDelegate)
+            {
+                pDelegate.keyUp(keys);
+            });
+        }
         this._m_bLocked = false;
         if (this._m_bToRemove)
         {
             this._m_bToRemove = false;
-            for (var i = 0; i < this._m_pHandlersToRemove.num; ++i)
+            for (var i = 0; i < this._m_pHandlersToRemove.length; ++i)
             {
-                this.forceRemoveDelegate(this._m_pHandlersToRemove.arr[i]);
+                this.forceRemoveDelegate(this._m_pHandlersToRemove[i]);
             }
             delete this._m_pHandlersToRemove;
             this._m_pHandlersToRemove = [];
@@ -159,11 +260,10 @@ cc.KeypadDispatcher = cc.Class.extend({
         if (this._m_bToAdd)
         {
             this._m_bToAdd = false;
-            for (var i = 0; i < this._m_pHandlersToAdd.num; ++i)
+            for (var i = 0; i < this._m_pHandlersToAdd.length; ++i)
             {
-                this.forceAddDelegate(this._m_pHandlersToAdd.arr[i]);
+                this.forceAddDelegate(this._m_pHandlersToAdd[i]);
             }
-            delete this._m_pHandlersToAdd;
             this._m_pHandlersToAdd = [];
         }
 
@@ -171,7 +271,6 @@ cc.KeypadDispatcher = cc.Class.extend({
     },
 
     //private
-    _KeypadDelegateArray: [],
     _m_pDelegates: [],
     _m_bLocked: false,
     _m_bToAdd: false,
@@ -187,6 +286,8 @@ cc.KeypadDispatcher.sharedDispatcher = function()
     if(! cc.s_KeypadDispatcher)
     {
         cc.s_KeypadDispatcher = new cc.KeypadDispatcher();
+        window.addEventListener("keydown", function(e){cc.s_KeypadDispatcher.dispatchKeypadMSG(e, true);});
+        window.addEventListener("keyup", function(e){cc.s_KeypadDispatcher.dispatchKeypadMSG(e, false);});
     }
     return cc.s_KeypadDispatcher;
 };
