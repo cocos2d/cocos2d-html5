7dc8d0373e770b83629d4d334e396af443d6e750
ShengxiangChen <shengxiang.chen@cocos2d-x.org>
2012-04-11 12:54:08 +0800

    fixed #1102: Implement CCTMXTiledMap

--- tests/cocos2d.js
+++ tests/cocos2d.js
@@ -116,10 +116,6 @@ cc.loadjs('actions/CCActionManager.js');//7
 cc.loadjs('layers_scenes_transitions_nodes/CCScene.js');//8
 cc.loadjs('layers_scenes_transitions_nodes/CCLayer.js');//9
 cc.loadjs('layers_scenes_transitions_nodes/CCTransition.js');//9
-cc.loadjs('tileMap_parallax_nodes/CCTMXTiledMap.js');
-cc.loadjs('tileMap_parallax_nodes/CCTMXXMLParser.js');
-cc.loadjs('tileMap_parallax_nodes/CCTMXObjectGroup.js');
-cc.loadjs('tileMap_parallax_nodes/CCTMXLayer.js');
 cc.loadjs('sprite_nodes/CCSprite.js');//10
 cc.loadjs('sprite_nodes/CCSpriteBatchNode.js');//10
 cc.loadjs('label_nodes/CCLabelTTF.js');//11
