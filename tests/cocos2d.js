0aed5fa2ea5dc2be8f9f54b37c94e819dfb1b977
ShengxiangChen <shengxiang.chen@cocos2d-x.org>
2012-04-11 11:45:24 +0800

    fixed #1102: Implement CCTMXTiledMap

--- tests/cocos2d.js
+++ tests/cocos2d.js
@@ -96,6 +96,9 @@ cc.loadjs = function(filename)
 cc.loadjs('platform/CCClass.js');//0
 cc.loadjs('platform/CCCommon.js');//1
 cc.loadjs('platform/platform.js');//2
+cc.loadjs('platform/JXGUtil.js');//4
+cc.loadjs('platform/base64.js');//4
+cc.loadjs('platform/gzip.js');//4
 cc.loadjs('cocoa/CCGeometry.js');//3
 cc.loadjs('cocoa/CCSet.js');//4
 cc.loadjs('platform/CCTypes.js');//5
@@ -113,6 +116,10 @@ cc.loadjs('actions/CCActionManager.js');//7
 cc.loadjs('layers_scenes_transitions_nodes/CCScene.js');//8
 cc.loadjs('layers_scenes_transitions_nodes/CCLayer.js');//9
 cc.loadjs('layers_scenes_transitions_nodes/CCTransition.js');//9
+cc.loadjs('tileMap_parallax_nodes/CCTMXTiledMap.js');
+cc.loadjs('tileMap_parallax_nodes/CCTMXXMLParser.js');
+cc.loadjs('tileMap_parallax_nodes/CCTMXObjectGroup.js');
+cc.loadjs('tileMap_parallax_nodes/CCTMXLayer.js');
 cc.loadjs('sprite_nodes/CCSprite.js');//10
 cc.loadjs('label_nodes/CCLabelTTF.js');//11
 cc.loadjs('touch_dispatcher/CCTouchDelegateProtocol.js');//12
