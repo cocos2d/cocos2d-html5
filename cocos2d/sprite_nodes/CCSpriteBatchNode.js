93208e530c02ac0f4ad131905426c26b0e97f17c
dingpinglv <dingping.lv@cocos2d-x.org>
2012-06-01 16:04:23 +0800

    fixed #1092 refactoring cc.SpriteBatchNode's code for Performance

--- cocos2d/sprite_nodes/CCSpriteBatchNode.js
+++ cocos2d/sprite_nodes/CCSpriteBatchNode.js
@@ -426,14 +426,14 @@ cc.SpriteBatchNode = cc.Node.extend({
                     //add dirty region
                     this._renderTexture.clear();
                     this._renderTexture.context.translate(this._anchorPointInPixels.x, -this._anchorPointInPixels.y);
+
                     if (this._children) {
                         for (var i = 0; i < this._children.length; i++) {
                             if (this._children[i]) {
-                                this._children[i].visit(context);
+                                this._children[i].visit(this._renderTexture.context);
                             }
                         }
                     }
-
                     this._isCacheDirty = false;
                 }
                 // draw RenderTexture
@@ -447,6 +447,7 @@ cc.SpriteBatchNode = cc.Node.extend({
                     }
                 }
             }
+
 
             if (this._grid && this._grid.isActive()) {
                 this._grid.afterDraw(this);
