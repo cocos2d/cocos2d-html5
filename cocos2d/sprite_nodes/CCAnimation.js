8bfc12930baf9d4ab39f291928b3ba5d85cfb06e
dingpinglv <dingping.lv@cocos2d-x.org>
2012-06-12 17:37:31 +0800

    issue #1306 add comments to cc.Animation

--- cocos2d/sprite_nodes/CCAnimation.js
+++ cocos2d/sprite_nodes/CCAnimation.js
@@ -23,55 +23,97 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  ****************************************************************************/
-var cc = cc = cc || {};
 
-/** A CCAnimation object is used to perform animations on the CCSprite objects.
-
- The CCAnimation object contains CCSpriteFrame objects, and a possible delay between the frames.
- You can animate a CCAnimation object by using the CCAnimate action. Example:
-
- [sprite runAction:[CCAnimate actionWithAnimation:animation]];
-
+/**
+ * <p>
+ *     A cc.Animation object is used to perform animations on the cc.Sprite objects.<br/>
+ *     <br/>
+ *      The cc.Animation object contains cc.SpriteFrame objects, and a possible delay between the frames. <br/>
+ *      You can animate a cc.Animation object by using the cc.Animate action. Example:  <br/>
+ * </p>
+ * @class
+ * @extends cc.Class
+ *
+ * @example
+ * //create an animation object
+ * var animation = cc.Animation.create();
+ *
+ * //add a sprite frame to this animation
+ * animation.addFrameWithFileName("grossini_dance_01.png");
+ *
+ * //create an animate with this animation
+ * var action = cc.Animate.create(3, animation, false);
+ *
+ * //run animate
+ * this._grossini.runAction(action);
  */
-cc.Animation = cc.Class.extend({
+cc.Animation = cc.Class.extend(/** @lends cc.Animation# */{
     _name:"",
     _delay:0,
     _frames:null,
+
+    /**
+     * @Constructor
+     */
     ctor:function () {
         this._frames = [];
     },
+
     // attributes
-    /** get name of the animation */
+    /**
+     * get name of the animation
+     * @return {String}
+     */
     getName:function () {
         return this._name;
     },
-    /** set name of the animation */
+
+    /**
+     * set name of the animation
+     * @param {String} name
+     */
     setName:function (name) {
         this._name = name;
     },
 
-    /** get delay between frames in seconds */
+    /**
+     * get delay between frames in seconds
+     * @return {Number}
+     */
     getDelay:function () {
         return this._delay;
     },
-    /** set delay between frames in seconds */
+
+    /**
+     * set delay between frames in seconds
+     * @param {Number} delay
+     */
     setDelay:function (delay) {
         this._delay = delay;
     },
 
-    /** get array of frames */
+    /**
+     * get array of frames
+     * @return {Array}
+     */
     getFrames:function () {
         return this._frames;
     },
-    /** set array of frames, the Frames is retained */
+
+    /**
+     * set array of frames, the Frames is retained
+     * @param {Array} frames
+     */
     setFrames:function (frames) {
         this._frames = frames;
     },
 
-    /** Initializes a CCAnimation with frames and a delay between frames
-     @since v0.99.5
+    /**
+     * Initializes a cc.Animation with frames and a delay between frames
+     * @param {Array} frames
+     * @param {Number} delay
+     * @return {Boolean}
      */
-
     initWithFrames:function (frames, delay) {
         this._delay = delay;
         if (frames)
@@ -82,13 +124,17 @@ cc.Animation = cc.Class.extend({
         return true;
     },
 
-    /** adds a frame to a CCAnimation */
+    /**
+     * adds a frame to a cc.Animation
+     * @param {cc.SpriteFrame} frame
+     */
     addFrame:function (frame) {
         this._frames.push(frame);
     },
 
-    /** Adds a frame with an image filename. Internally it will create a CCSpriteFrame and it will add it.
-     Added to facilitate the migration from v0.8 to v0.9.
+    /**
+     * Adds a frame with an image filename. Internally it will create a cc.SpriteFrame and it will add it.
+     * @param {String} fileName
      */
     addFrameWithFileName:function (fileName) {
         var texture = cc.TextureCache.sharedTextureCache().addImage(fileName);
@@ -104,8 +150,10 @@ cc.Animation = cc.Class.extend({
         this._frames.push(frame);
     },
 
-    /** Adds a frame with a texture and a rect. Internally it will create a CCSpriteFrame and it will add it.
-     Added to facilitate the migration from v0.8 to v0.9.
+    /**
+     * Adds a frame with a texture and a rect. Internally it will create a CCSpriteFrame and it will add it.
+     * @param {cc.Texture2D} texture
+     * @param {cc.Rect} rect
      */
     addFrameWithTexture:function (texture, rect) {
         var pFrame = cc.SpriteFrame.create(texture, rect);
@@ -117,8 +165,23 @@ cc.Animation = cc.Class.extend({
     }
 });
 
-
-/* Creates an animation .
+/**
+ * Creates an animation.
+ * @param {Array} frames
+ * @param {Number} delay
+ * @return {cc.Animation}
+ * @example
+ * //Creates an animation
+ * var animation1 = cc.Animation.create();
+ *
+ * //Create an animation with sprite frames
+ * var animFrames = [];
+ * var frame = cache.spriteFrameByName("grossini_dance_01.png");
+ * animFrames.push(frame);
+ * var animation2 = cc.Animation.create(animFrames);
+ *
+ * //Create an animation with sprite frames and delay
+ * var animation3 = cc.Animation.create(animFrames, 0.2);
  */
 cc.Animation.create = function (frames, delay) {
     var animation = new cc.Animation();
@@ -131,6 +194,5 @@ cc.Animation.create = function (frames, delay) {
         }
         animation.initWithFrames(frames, delay);
     }
-
     return animation;
 };
