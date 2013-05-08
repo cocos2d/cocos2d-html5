75dafa6409e4814c0fbac9ef40b8d1ee81b8abd1
dingpinglv <dingping.lv@cocos2d-x.org>
2013-05-08 09:57:38 +0800

    issue #2082 fixing a bug of AnimationTest in CocosBuilderTest

--- HelloHTML5World/src/resource.js
+++ HelloHTML5World/src/resource.js
@@ -6,7 +6,10 @@ var g_ressources = [
     //image
     {src:s_HelloWorld},
     {src:s_CloseNormal},
-    {src:s_CloseSelected}
+    {src:s_CloseSelected},
+
+    {src:"res/grossini.plist"},
+    {src:"res/grossini.png"}
 
     //plist
 
