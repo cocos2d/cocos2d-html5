fdcad58c1cb5dff3ec6369dbd9c9a2298fefa930
xingsenma <xingsen.ma@cocos2d-x.org>
2013-07-12 16:41:10 +0800

    fixed #2299 add json to cc.RESOURCE_TYPE

--- cocos2d/CCLoader.js
+++ cocos2d/CCLoader.js
@@ -35,7 +35,7 @@ cc.RESOURCE_TYPE = {
     "XML":["plist", "xml", "fnt", "tmx", "tsx"],
     "BINARY":["ccbi"],
     "FONT":"FONT",
-    "TEXT":["txt", "vsh", "fsh"],
+    "TEXT":["txt", "vsh", "fsh","json"],
     "UNKNOW":[]
 };
 
