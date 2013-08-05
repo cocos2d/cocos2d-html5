fd27bb73f0537b59fe321cfcce2d298067aeb75f
dingpinglv <dingping.lv@cocos2d-x.org>
2013-08-05 15:48:13 +0800

    fixed #2468 Corrected a mistake of cc.NodeLoader

--- extensions/CCBReader/CCNodeLoader.js
+++ extensions/CCBReader/CCNodeLoader.js
@@ -491,9 +491,10 @@ cc.NodeLoader = cc.Class.extend({
                 var texture = cc.TextureCache.getInstance().addImage(spriteFile);
                 var bounds;
                 if(texture instanceof  cc.Texture2D){
-                    bounds = cc.RectMake(0, 0, texture.getContentSize().width, texture.getContentSize().height);
+                    var locContentSize = texture.getContentSize();
+                    bounds = cc.RectMake(0, 0, locContentSize.width, locContentSize.height);
                 }else{
-                    bounds = cc.RECT_PIXELS_TO_POINTS(bounds);
+                    bounds = cc.RECT_PIXELS_TO_POINTS(cc.RectMake(0, 0, texture.width, texture.height));
                 }
                 spriteFrame = cc.SpriteFrame.createWithTexture(texture, bounds);
             } else {
