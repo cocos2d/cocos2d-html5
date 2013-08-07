eeaef0222157798e048c9360e4967dabcad2621d
NeroChan <shengxiang.chen@cocos2d-x.org>
2013-08-07 17:04:22 +0800

    fixed #2497 : setSearchPaths Bug

--- cocos2d/sprite_nodes/CCSprite.js
+++ cocos2d/sprite_nodes/CCSprite.js
@@ -929,6 +929,7 @@ cc.SpriteCanvas = cc.NodeRGBA.extend(/** @lends cc.SpriteCanvas# */{
         var selfPointer = this;
         var texture = cc.TextureCache.getInstance().textureForKey(filename);
         if (!texture) {
+            filename = cc.FileUtils.getInstance().fullPathForFilename(filename);
             this._visible = false;
             var loadImg = new Image();
             loadImg.addEventListener("load", function () {
@@ -2127,6 +2128,7 @@ cc.SpriteWebGL = cc.NodeRGBA.extend(/** @lends cc.SpriteWebGL# */{
 
         var texture = cc.TextureCache.getInstance().textureForKey(filename);
         if (!texture) {
+            filename = cc.FileUtils.getInstance().fullPathForFilename(filename);
             this._visible = false;
             var loadImg = new Image();
             loadImg.addEventListener("load", function () {
