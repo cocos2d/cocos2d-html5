a861869ce5031f719c7c03506dca88311f88d24d
06wj <06wj@163.com>
2013-08-26 17:19:09 +0800

    added js loading image

--- cocos2d/platform/jsloader.js
+++ cocos2d/platform/jsloader.js
@@ -38,7 +38,7 @@
         'platform/CCMacro.js',
         'platform/CCFileUtils.js',
         'platform/CCTypes.js',
-		'platform/CCAccelerometer.js',
+        'platform/CCAccelerometer.js',
         'platform/zlib.min.js',
         'platform/CCEGLView.js',
         'platform/CCImage.js',
@@ -248,6 +248,27 @@
 
     }
 
+    var loadJsImg = document.getElementById("cocos2d_loadJsImg");
+    if(!loadJsImg){
+        loadJsImg = new Image();
+        loadJsImg.src = "data:image/gif;base64,R0lGODlhEAAQALMNAD8/P7+/vyoqKlVVVX9/fxUVFUBAQGBgYMDAwC8vL5CQkP///wAAAP///wAAAAAAACH/C05FVFNDQVBFMi4wAwEAAAAh+QQFAAANACwAAAAAEAAQAAAEO5DJSau9OOvNex0IMnDIsiCkiW6g6BmKYlBFkhSUEgQKlQCARG6nEBwOgl+QApMdCIRD7YZ5RjlGpCUCACH5BAUAAA0ALAAAAgAOAA4AAAQ6kLGB0JA4M7QW0hrngRllkYyhKAYqKUGguAws0ypLS8JxCLQDgXAIDg+FRKIA6v0SAECCBpXSkstMBAAh+QQFAAANACwAAAAACgAQAAAEOJDJORAac6K1kDSKYmydpASBUl0mqmRfaGTCcQgwcxDEke+9XO2WkxQSiUIuAQAkls0n7JgsWq8RACH5BAUAAA0ALAAAAAAOAA4AAAQ6kMlplDIzTxWC0oxwHALnDQgySAdBHNWFLAvCukc215JIZihVIZEogDIJACBxnCSXTcmwGK1ar1hrBAAh+QQFAAANACwAAAAAEAAKAAAEN5DJKc4RM+tDyNFTkSQF5xmKYmQJACTVpQSBwrpJNteZSGYoFWjIGCAQA2IGsVgglBOmEyoxIiMAIfkEBQAADQAsAgAAAA4ADgAABDmQSVZSKjPPBEDSGucJxyGA1XUQxAFma/tOpDlnhqIYN6MEAUXvF+zldrMBAjHoIRYLhBMqvSmZkggAIfkEBQAADQAsBgAAAAoAEAAABDeQyUmrnSWlYhMASfeFVbZdjHAcgnUQxOHCcqWylKEohqUEAYVkgEAMfkEJYrFA6HhKJsJCNFoiACH5BAUAAA0ALAIAAgAOAA4AAAQ3kMlJq704611SKloCAEk4lln3DQgyUMJxCBKyLAh1EMRR3wiDQmHY9SQslyIQUMRmlmVTIyRaIgA7";
+
+        var canvasNode = document.getElementById(c.tag);
+        canvasNode.style.backgroundColor = "black";
+        canvasNode.parentNode.appendChild(loadJsImg);
+        
+        var canvasStyle = getComputedStyle?getComputedStyle(canvasNode):canvasNode.currentStyle;
+        loadJsImg.style.left = canvasNode.offsetLeft + (parseFloat(canvasStyle.width) - loadJsImg.width)/2 + "px";
+        loadJsImg.style.top = canvasNode.offsetTop + (parseFloat(canvasStyle.height) - loadJsImg.height)/2 + "px";
+        loadJsImg.style.position = "absolute";
+    }
+    
+    var updateLoading = function(p){
+        if(p>=1) {
+            loadJsImg.parentNode.removeChild(loadJsImg);
+        }
+    }
+
     var loaded = 0;
     var que = engine.concat(c.appFiles);
     que.push('main.js');
@@ -263,8 +284,9 @@
                 f.onload = loadNext;
                 d.body.appendChild(f);
                 //TODO: code for updating progress bar
-                //p = s / (que.length - 1);
             }
+            var p = s / (que.length - 1);
+            updateLoading(p);
         };
         loadNext();
     }
@@ -276,7 +298,8 @@
             s.onload = function () {
                 loaded++;
                 //TODO: code for updating progress bar
-                //p = loaded / que.length;
+                var p = loaded / que.length;
+                updateLoading(p);
             };
             d.body.appendChild(s);
             que[i] = s;
