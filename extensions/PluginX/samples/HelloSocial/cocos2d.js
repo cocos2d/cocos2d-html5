4da58f1e71945aad618ea2f19bac64da2054cf30
dingpinglv <dingping.lv@cocos2d-x.org>
2013-12-20 17:35:43 +0800

    Closed #3371: modify jsloader.js path

--- extensions/PluginX/samples/HelloSocial/cocos2d.js
+++ extensions/PluginX/samples/HelloSocial/cocos2d.js
@@ -68,7 +68,7 @@
             s.src = c.SingleEngineFile;
         }
         else if (c.engineDir && !c.SingleEngineFile) {
-            s.src = c.engineDir + 'platform/jsloader.js';
+            s.src = c.engineDir + 'jsloader.js';
         }
         else {
             alert('You must specify either the single engine file OR the engine directory in "cocos2d.js"');
