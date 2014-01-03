89f7befeb34459c3f5a8f3d75f204683bb02d8da
SmallJun <536762164@qq.com>
2014-01-03 10:56:07 +0800

    issue #3541 : Pretty for test case.

--- node_modules/ccgui/main.js
+++ node_modules/ccgui/main.js
@@ -5,24 +5,34 @@ var testTitle = document.getElementById("testTitle");
 var testJsPath = document.getElementById("testJsPath");
 var testConsole = document.getElementById("testConsole");
 var custDiv = document.getElementById("custDiv");
+var scriptContent = document.getElementById("scriptContent");
+var menuUl = document.getElementById("menuUl");
 
 var testCaseIndex = -1;
+var isCurr = false;
 
 function playTestCase(flag){
-    if(typeof cc == "undefined") return;
-    if(typeof cc.resCfg == "undefined") return;
-    if(typeof cc.gameModules == "undefined") return;
     var resCfg = cc.resCfg;
     var gms = cc.gameModules;
+    isCurr = flag == 0;
     testCaseIndex += flag;
     testCaseIndex = testCaseIndex >= gms.length ? 0 : testCaseIndex;
     testCaseIndex = testCaseIndex < 0 ? gms.length - 1 : testCaseIndex;
     var cfgName = gms[testCaseIndex];
-    var cfg = resCfg[cfgName];
+    showTestCase(cfgName, resCfg[cfgName]);
+}
+
+function showTestCase(cfgName, cfg){
     testTitle.innerHTML = cfg.title || "";
     testJsPath.innerHTML = cfgName.replace(/\[\%[\w\d\-_]*\%\]/, "");
     clearTextConsole();
-    cc.test(cfgName, flag == 0 ? cc.Loader : null);
+    getFileContent(testJsPath.innerHTML, function(content){
+        scriptContent.innerHTML = content;
+        scriptContent.className = "prettyprint";
+        prettyPrint();
+    });
+    cc.test(cfgName, isCurr ? cc.Loader : null);
+    isCurr = false;
 }
 
 preBtn.addEventListener("click", function(){
@@ -44,6 +54,70 @@ function logTest(msg){
 function clearTextConsole(){
     if(!testConsole.hidden) testConsole.hidden = "hidden";
     testConsole.value = "TextConsole...\r\n";
+};
+
+function createBtnContainer(parent){
+    var div = document.createElement("div");
+    div.className = "test-btn-container";
+    if(parent) parent.appendChild(div);
+    return div;
+};
+
+function createBtn(container, text, func){
+    var btn = document.createElement("button");
+    btn.innerHTML = text;
+    btn.addEventListener("click", func);
+    btn.className = "btn btn_black";
+    if(container) container.appendChild(btn);
+    return btn;
+};
+
+function createMenuItem(cfgName, cfg, index){
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    li.appendChild(a);
+    a.href = "javascript:;";
+    a.innerHTML = cfg.layer;
+    a.addEventListener("click", function(){
+        isCurr = testCaseIndex == index;
+        testCaseIndex = index;
+        showTestCase(cfgName, cfg);
+    });
+    menuUl.appendChild(li);
+}
+
+function getFileContent(url, callback) {
+    var httpRequest;
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+        httpRequest = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { // IE
+        try {
+            httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+        }
+        catch (e) {
+            try {
+                httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            catch (e) {}
+        }
+    }
+    if (!httpRequest) return false;
+
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) callback(httpRequest.responseText);
+            else callback(null);
+        }
+    };
+
+    httpRequest.open('GET', url, true);
+    httpRequest.send();
+}
+
+var gms = cc.gameModules;
+for(var i = 0, li = gms.length; i < li; i++){
+    var cfgName = gms[i];
+    createMenuItem(cfgName, cc.resCfg[cfgName], i);
 }
 
 var cocos2dApp = cc.Application.extend({
