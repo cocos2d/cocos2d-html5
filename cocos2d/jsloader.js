7857e84310e560acbf67b1d7801f59765bfdd3ae
pandamicro <lphuabin@gmail.com>
2014-01-14 17:34:12 +0800

    Move CCFormatHelper’s content into CCFileUtils
    In addition:
    CCEGLView: Use frame to request full screen instead of canvas

--- cocos2d/jsloader.js
+++ cocos2d/jsloader.js
@@ -112,7 +112,6 @@
         'compress/base64.js',
         'compress/gzip.js',
         'compress/zlib.min.js',
-        'particle_nodes/CCFormatHelper.js',
         'particle_nodes/CCPNGReader.js',
         'particle_nodes/CCTIFFReader.js',
         'particle_nodes/CCParticleSystem.js',
