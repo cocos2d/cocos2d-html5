87e648a8222c0a63ec28b418c33564db20678131
pandamicro <lphuabin@gmail.com>
2014-01-30 12:04:47 +0800

    Refactor #3878: Refactor position getter/setter in engine Vol.1

--- cocos2d/actions/CCActionInstant.js
+++ cocos2d/actions/CCActionInstant.js
@@ -331,7 +331,8 @@ cc.Place = cc.ActionInstant.extend(/** @lends cc.Place# */{
      * @param {Number} time
      */
     update:function (time) {
-        this._target.setPosition(this._position);
+        this._target.x = this._position.x;
+	    this._target.y = this._position.y;
     },
 
     clone:function(){
--- cocos2d/core/CCDirector.js
+++ cocos2d/core/CCDirector.js
@@ -1154,9 +1154,9 @@ cc.Director = cc.Class.extend(/** @lends cc.Director# */{
         this._drawsLabel = tmpLabel;
 
         var locStatsPosition = cc.DIRECTOR_STATS_POSITION;
-        this._drawsLabel.setPosition(cc.pAdd(cc.p(0, 34 * factor), locStatsPosition));
-        this._SPFLabel.setPosition(cc.pAdd(cc.p(0, 17 * factor), locStatsPosition));
-        this._FPSLabel.setPosition(locStatsPosition);
+        this._drawsLabel.pos = cc.pAdd(cc.p(0, 34 * factor), locStatsPosition);
+        this._SPFLabel.pos = cc.pAdd(cc.p(0, 17 * factor), locStatsPosition);
+        this._FPSLabel.pos = locStatsPosition;
     },
 
     _createStatsLabelForCanvas:function(){
