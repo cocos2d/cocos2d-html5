837ca94dd3e219cac51001629f3e0f0fc8d33b36
dingpinglv <dingping.lv@cocos2d-x.org>
2014-03-20 17:25:27 +0800

    Fixed #4453: remove some invalid codes of cc.EditBox

--- extensions/editbox/CCEditBox.js
+++ extensions/editbox/CCEditBox.js
@@ -633,21 +633,18 @@ cc.EditBox.getRect = function (node) {
 /**
  * create a edit box with size and background-color or
  * @param {cc.Size} size
- * @param {cc.Color | cc.Scale9Sprite } normal9SpriteBg
+ * @param {cc.Scale9Sprite } normal9SpriteBg
+ * @param {cc.Scale9Sprite } [press9SpriteBg]
+ * @param {cc.Scale9Sprite } [disabled9SpriteBg]
  */
 cc.EditBox.create = function (size, normal9SpriteBg, press9SpriteBg, disabled9SpriteBg) {
     var edbox = new cc.EditBox(size);
-    if (normal9SpriteBg instanceof cc.Color) {
-        edbox.setBackgroundColor(normal9SpriteBg);
-    } else {
-        //Todo
-        if (edbox.initWithSizeAndBackgroundSprite(size, normal9SpriteBg)) {
-            if (press9SpriteBg)
-                edbox.setBackgroundSpriteForState(press9SpriteBg, cc.CONTROL_STATE_HIGHLIGHTED);
+    if (edbox.initWithSizeAndBackgroundSprite(size, normal9SpriteBg)) {
+        if (press9SpriteBg)
+            edbox.setBackgroundSpriteForState(press9SpriteBg, cc.CONTROL_STATE_HIGHLIGHTED);
 
-            if (disabled9SpriteBg)
-                edbox.setBackgroundSpriteForState(disabled9SpriteBg, cc.CONTROL_STATE_DISABLED);
-        }
+        if (disabled9SpriteBg)
+            edbox.setBackgroundSpriteForState(disabled9SpriteBg, cc.CONTROL_STATE_DISABLED);
     }
     return edbox;
 };
