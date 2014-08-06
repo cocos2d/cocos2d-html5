ff27a7dced9976f9eae9b752665ade9509a4b1e8
dingpinglv <dingping.lv@cocos2d-x.org>
2014-08-06 22:36:14 +0800

    Fixed #5781: fixed a bug of ccui.Layout

--- extensions/cocostudio/armature/display/CCSkin.js
+++ extensions/cocostudio/armature/display/CCSkin.js
@@ -113,8 +113,7 @@ ccs.Skin = ccs.Sprite.extend(/** @lends ccs.Skin# */{
             var transform = this.getNodeToParentTransform ? this.getNodeToParentTransform() : this.nodeToParentTransform();
             var size = this._rect;
 
-            var x1 = this._offsetPosition.x;
-            var y1 = this._offsetPosition.y;
+            var x1 = this._offsetPosition.x, y1 = this._offsetPosition.y;
 
             var x2 = x1 + size.width, y2 = y1 + size.height;
             var x = transform.tx, y = transform.ty;
@@ -203,7 +202,7 @@ ccs.Skin = ccs.Sprite.extend(/** @lends ccs.Skin# */{
 if (cc._renderType == cc._RENDER_TYPE_WEBGL) {
     ccs.Skin.prototype.updateTransform = ccs.Skin.prototype._updateTransformForWebGL;
 }else{
-    //ccs.Skin.prototype.updateTransform = cc.Sprite.prototype.updateTransform;
+    //ccs.Skin.prototype.getNodeToParentTransform = cc.Node.prototype._getNodeToParentTransformForWebGL;
 }
 //ccs.Skin.prototype.nodeToParentTransform = cc.Node.prototype._getNodeToParentTransformForWebGL;
 
