3c23c5a7c485eb9cfb50bd1cd439da4741cb3722
dingpinglv <dingping.lv@cocos2d-x.org>
2014-08-20 11:09:12 +0800

    Closed #5832: Fixed a bug of RenderTexture that it doesn't support depthStencilFormat in v2.x

--- cocos2d/render_texture/CCRenderTexture.js
+++ cocos2d/render_texture/CCRenderTexture.js
@@ -235,16 +235,18 @@ cc.RenderTexture = cc.Node.extend(/** @lends cc.RenderTexture# */{
         // associate texture with FBO
         gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, locTexture._webTextureObj, 0);
 
-        if (depthStencilFormat != 0) {
+        if (depthStencilFormat != null && depthStencilFormat != 0) {
             //create and attach depth buffer
             this._depthRenderBuffer = gl.createRenderbuffer();
             gl.bindRenderbuffer(gl.RENDERBUFFER, this._depthRenderBuffer);
             gl.renderbufferStorage(gl.RENDERBUFFER, depthStencilFormat, powW, powH);
-            gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, this._depthRenderBuffer);
 
-            // if depth format is the one with stencil part, bind same render buffer as stencil attachment
-            //if (depthStencilFormat == gl.DEPTH24_STENCIL8)
-            //    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, this._depthRenderBuffer);
+            if(depthStencilFormat == gl.DEPTH_STENCIL)
+                gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_STENCIL_ATTACHMENT, gl.RENDERBUFFER, this._depthRenderBuffer);
+            else if(depthStencilFormat == gl.STENCIL_INDEX || depthStencilFormat == gl.STENCIL_INDEX8)
+                gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.STENCIL_ATTACHMENT, gl.RENDERBUFFER, this._depthRenderBuffer);
+            else if(depthStencilFormat == gl.DEPTH_COMPONENT16)
+                gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, this._depthRenderBuffer);
         }
 
         // check if it worked (probably worth doing :) )
