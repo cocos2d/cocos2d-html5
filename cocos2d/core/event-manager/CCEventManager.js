da04f7f993aa1e715f7e0fd8d5eec9ad7a4182b1
dingpinglv <dingping.lv@cocos2d-x.org>
2014-08-25 17:03:03 +0800

    Fixed #5845: Fixed a bug of InputManager, its first touch move event's delta is wrong.

--- cocos2d/core/event-manager/CCEventManager.js
+++ cocos2d/core/event-manager/CCEventManager.js
@@ -320,7 +320,7 @@ cc.eventManager = /** @lends cc.eventManager# */{
     _sortEventListenersOfSceneGraphPriorityDes : function(l1, l2){
         var locNodePriorityMap = cc.eventManager._nodePriorityMap;
         if(!l1 || !l2 || !l1._getSceneGraphPriority() || !l2._getSceneGraphPriority())
-            debugger;
+            return -1;
         return locNodePriorityMap[l2._getSceneGraphPriority().__instanceId] - locNodePriorityMap[l1._getSceneGraphPriority().__instanceId];
     },
 
