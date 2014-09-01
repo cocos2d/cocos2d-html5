058e5e217c5095f2be357645a7d7448c84d613e6
Minh Quy <sugiacupit@gmail.com>
2014-09-01 12:07:59 +0700

    Consider using function to check type of variable
    I think we should we function to check type of variable like underscore or lodash to make it more flexible or easier to test

--- CCBoot.js
+++ CCBoot.js
@@ -104,6 +104,63 @@ cc.extend = function(target) {
 };
 
 /**
+ * Check the obj whether is function or not
+ * @param {*} obj
+ * @returns {boolean}
+ */
+cc.isFunction = function(obj) {
+    return typeof obj == 'function';
+};
+
+/**
+ * Check the obj whether is number or not
+ * @param {*} obj
+ * @returns {boolean}
+ */
+cc.isNumber = function(obj) {
+    return typeof obj == 'number' || Object.prototype.toString.call(obj) == '[object Number]';
+};
+
+/**
+ * Check the obj whether is string or not
+ * @param {*} obj
+ * @returns {boolean}
+ */
+cc.isString = function(obj) {
+    return typeof obj == 'string' || Object.prototype.toString.call(obj) == '[object String]';
+};
+
+/**
+ * Check the obj whether is array or not
+ * @param {*} obj
+ * @returns {boolean}
+ */
+cc.isArray = function(obj) {
+    return Object.prototype.toString.call(obj) == '[object Array]';
+};
+
+/**
+ * Check the obj whether is undefined or not
+ * @param {*} obj
+ * @returns {boolean}
+ */
+
+cc.isUndefined = function(obj) {
+    return typeof obj == 'undefined';
+};
+
+/**
+ * Check the obj whether is object or not
+ * @param {*} obj
+ * @returns {boolean}
+ */
+cc.isObject = function(obj) {
+    var type = typeof obj;
+
+    return type == 'function' || (obj && type == 'object');
+}
+
+/**
  * Check the url whether cross origin
  * @param {String} url
  * @returns {boolean}
