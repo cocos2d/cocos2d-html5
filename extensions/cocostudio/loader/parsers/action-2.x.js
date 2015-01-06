f69ff3151ed15a504c38e4eaafd3a473e171895d
VisualSJ <itharbors@outlook.com>
2015-01-06 14:23:33 +0800

    Issue #2563: support timeline(action) 2.0.0.0

--- extensions/cocostudio/loader/parsers/action-2.x.js
+++ extensions/cocostudio/loader/parsers/action-2.x.js
@@ -1,5 +1,7 @@
 (function(load, baseParser){
 
+    var cache = {};
+
     var Parser = baseParser.extend({
 
         getNodeJson: function(json){
@@ -7,21 +9,193 @@
         },
 
         parseNode: function(json, resourcePath, file){
-            var self = this;
+            if(cache[file])
+                return cache[file].clone();
+
+            var self = this,
+                action = new ccs.ActionTimeline();
+
+            action.setDuration(json["Duration"]);
+            action.setTimeSpeed(json["Speed"] || 1);
             //The process of analysis
             var timelines = json["Timelines"];
             timelines.forEach(function(timeline){
-                var parser = self.parsers[timeline["FrameType"]];
-                parser.call(self, timeline, resourcePath);
+                var parser = self.parsers[timeline["Property"]];
+                var frame;
+                if(parser)
+                    frame = parser.call(self, timeline, resourcePath);
+                else
+                    cc.log("parser is not exists : %s", timeline["Property"]);
+                if(frame)
+                    action.addTimeline(frame);
             });
-            return new cc.Action();
+
+            cache[file] = action;
+
+            return action.clone();
         }
 
     });
     var parser = new Parser();
 
-    parser.registerParser("RotationSkewFrame", function(options, resourcePath){
+    var frameList = [
+        {
+            name: "Position",
+            handle: function(options){
+                var frame = new ccs.PositionFrame();
+                var x = options["X"];
+                var y = options["Y"];
+                frame.setPosition(cc.p(x,y));
+                return frame;
+            }
+        },
+        {
+            name: "VisibleForFrame",
+            handle: function(options){
+                var frame = new ccs.VisibleFrame();
+                var visible = options["Value"];
+                frame.setVisible(visible);
+                return frame;
+            }
+        },
+        {
+            name: "Scale",
+            handle: function(options){
+                var frame = new ccs.ScaleFrame();
+                var scalex = options["X"];
+                var scaley = options["Y"];
+                frame.setScaleX(scalex);
+                frame.setScaleY(scaley);
+                return frame;
+            }
+        },
+        {
+            name: "Rotation",
+            handle: function(options){
+                var frame = new ccs.RotationFrame();
+                var rotation = options["Rotation"];
+                frame.setRotation(rotation);
+                return frame;
+            }
+        },
+        {
+            name: "Skew",
+            handle: function(options){
+                var frame = new ccs.SkewFrame();
+                var skewx = options["X"];
+                var skewy = options["Y"];
+                frame.setSkewX(skewx);
+                frame.setSkewY(skewy);
+                return frame;
+            }
+        },
+        {
+            name: "RotationSkew",
+            handle: function(options){
+                var frame = new ccs.RotationSkewFrame();
+                var skewx = options["X"];
+                var skewy = options["Y"];
+                frame.setSkewX(skewx);
+                frame.setSkewY(skewy);
+                return frame;
+            }
+        },
+        {
+            name: "Anchor",
+            handle: function(options){
+                var frame = new ccs.AnchorPointFrame();
+                var anchorx = options["X"];
+                var anchory = options["Y"];
+                frame.setAnchorPoint(cc.p(anchorx, anchory));
+                return frame;
+            }
+        },
+        {
+            name: "InnerAction",
+            handle: function(options){
+                var frame = new ccs.InnerActionFrame();
+                var type = options["InnerActionType"];
+                var startFrame = options["StartFrame"];
+                frame.setInnerActionType(type);
+                frame.setStartFrameIndex(startFrame);
+                return frame;
+            }
+        },
+        {
+            name: "CColor",
+            handle: function(options){
+                var frame = new ccs.ColorFrame();
+                var color = options["Color"];
+                if(!color) color = {};
+                color["R"] = color["R"] || 255;
+                color["G"] = color["G"] || 255;
+                color["B"] = color["B"] || 255;
+                frame.setColor(cc.color(color["R"], color["G"], color["B"]));
+                return frame;
+            }
+        },
+        {
+            name: "Alpha",
+            handle: function(options){
+                var frame = new ccs.AlphaFrame();
+                var alpha = options["Value"];
+                frame.setAlpha(alpha);
+                return frame;
+            }
+        },
+        {
+            name: "FileData",
+            handle: function(options, resourcePath){
+                var frame = new ccs.TextureFrame();
+                var texture = options["TextureFile"];
+                if(texture != null) {
+                    var path = texture["Path"];
+                    var spriteFrame = cc.spriteFrameCache._spriteFrames[path];
+                    if(spriteFrame == null){
+                        path = resourcePath + path;
+                    }
+                    frame.setTextureName(path);
+                }
+                return frame;
+            }
+        },
+        {
+            name: "FrameEvent",
+            handle: function(options){
+                var frame = new ccs.EventFrame();
+                var evnt = options["Value"];
+                if(evnt != null)
+                    frame.setEvent(evnt);
+                return frame;
+            }
+        },
+        {
+            name: "ZOrder",
+            handle: function(options){
+                var frame = new ccs.ZOrderFrame();
+                var zorder = options["Value"];
+                frame.setZOrder(zorder);
+                return frame;
+            }
+        }
+    ];
 
+    frameList.forEach(function(item){
+        parser.registerParser(item.name, function(options, resourcePath){
+            var timeline = new ccs.Timeline();
+            timeline.setActionTag(options["ActionTag"]);
+
+            var frames = options["Frames"];
+            if(frames && frames.length){
+                frames.forEach(function(frameData){
+                    var frame = item.handle(frameData, resourcePath);
+                    frame.setFrameIndex(frameData["FrameIndex"]);
+                    frame.setTween(frameData["Tween"]);
+                    timeline.addFrame(frame);
+                });
+            }
+            return timeline;
+        });
     });
 
     load.registerParser("action", "2.*", parser);
