b44eefb1c63dd496b93adcbebc59fcfaab057099
VisualSJ <itharbors@outlook.com>
2015-01-07 14:18:14 +0800

    Issue #2563: Add a scene analysis

--- extensions/cocostudio/loader/load.js
+++ extensions/cocostudio/loader/load.js
@@ -1,3 +1,27 @@
+/****************************************************************************
+ Copyright (c) 2013-2014 Chukong Technologies Inc.
+
+ http://www.cocos2d-x.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
 ccs._load = (function(){
 
     /**
@@ -24,6 +48,8 @@ ccs._load = (function(){
                 parse = parser["timeline"];
             else if(json["Content"] && json["Content"]["Content"])
                 parse = parser["timeline"];
+            else if(json["gameobjects"] && json["Triggers"])
+                parse = parser["scene"];
         }else{
             parse = parser[type];
         }
@@ -45,7 +71,8 @@ ccs._load = (function(){
     var parser = {
         "ccui": {},
         "timeline": {},
-        "action": {}
+        "action": {},
+        "scene": {}
     };
 
     load.registerParser = function(name, version, target){
@@ -156,4 +183,33 @@ ccs.load = function(file){
         cc.log("ccs.load has encountered some problems");
     }
     return object;
+};
+
+//Forward compatible interface
+
+ccs.actionTimelineCache = {
+
+
+    //@deprecated This function will be deprecated sooner or later please use ccs.load
+    /**
+     * Create Timeline Action
+     * @param file
+     * @returns {*}
+     */
+    createAction: function(file){
+        return ccs._load(file, "action");
+    }
+};
+
+ccs.csLoader = {
+
+    //@deprecated This function will be deprecated sooner or later please use ccs.load
+    /**
+     * Create Timeline Node
+     * @param file
+     * @returns {*}
+     */
+    createNode: function(file){
+        return ccs._load(file);
+    }
 };
