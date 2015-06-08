2d87e02b62ddcb30cf9a6dfc1e2c148f0b5ae5c0
VisualSJ <itharbors@outlook.com>
2015-06-08 15:39:38 +0800

    Fixed control LayoutComponent positioning bug

--- extensions/cocostudio/loader/parsers/timelineParser-2.x.js
+++ extensions/cocostudio/loader/parsers/timelineParser-2.x.js
@@ -121,6 +121,8 @@
 
         node.setCascadeColorEnabled(true);
         node.setCascadeOpacityEnabled(true);
+
+        setLayoutComponent(node, json);
     };
 
     parser.parseChild = function(node, children, resourcePath){
@@ -326,6 +328,11 @@
         var color = json["CColor"];
         if (color != null)
             widget.setColor(getColor(color));
+
+        setLayoutComponent(widget, json);
+    };
+
+    var setLayoutComponent = function(widget, json){
 
         var layoutComponent = ccui.LayoutComponent.bindLayoutComponent(widget);
         if(!layoutComponent)
