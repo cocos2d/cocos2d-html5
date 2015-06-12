531d5aadf865b392dc00528ab2a35f076bd4db81
jianglong0156 <jianglong0156@gmail.com>
2015-06-12 10:25:00 +0800

    fix the prompt can not use in wechat

--- cocos2d/text-input/CCIMEDispatcher.js
+++ cocos2d/text-input/CCIMEDispatcher.js
@@ -389,7 +389,8 @@ cc.IMEDispatcher = cc.Class.extend(/**  @lends cc.imeDispatcher# */{
             this._currentInputString = delegate.string || "";
 
             var tipMessage = delegate.getTipMessage ? delegate.getTipMessage() : "please enter your word:";
-            var userInput = prompt(tipMessage, this._currentInputString);
+            // wechat cover the prompt funciton .So need use the Window.prototype.prompt
+            var userInput = Window.prototype.prompt.call(window, tipMessage, this._currentInputString);
             if(userInput != null)
                 this._processDomInputString(userInput);
             this.dispatchInsertText("\n", 1);
