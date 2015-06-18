a40351899c3bb997cb2a4f637575297ddd89fa30
VisualSJ <itharbors@outlook.com>
2015-06-18 14:55:47 +0800

    Fixed a bug that can't input

--- cocos2d/text-input/CCIMEDispatcher.js
+++ cocos2d/text-input/CCIMEDispatcher.js
@@ -390,7 +390,12 @@ cc.IMEDispatcher = cc.Class.extend(/**  @lends cc.imeDispatcher# */{
 
             var tipMessage = delegate.getTipMessage ? delegate.getTipMessage() : "please enter your word:";
             // wechat cover the prompt funciton .So need use the Window.prototype.prompt
-            var userInput = Window.prototype.prompt.call(window, tipMessage, this._currentInputString);
+            var userInput;
+            if(window.Window && Window.prototype.prompt != prompt){
+                userInput = Window.prototype.prompt.call(window, tipMessage, this._currentInputString);
+            }else{
+                userInput = prompt(tipMessage, this._currentInputString);
+            }
             if(userInput != null)
                 this._processDomInputString(userInput);
             this.dispatchInsertText("\n", 1);
