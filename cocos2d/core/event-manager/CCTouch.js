6e23a09f40a75bdc981faf52a13ec802ae196ad4
pandamicro <lphuabin@gmail.com>
2015-08-23 21:22:26 +0800

    Fixed CCTouch's startPoint unset issue

--- cocos2d/core/event-manager/CCTouch.js
+++ cocos2d/core/event-manager/CCTouch.js
@@ -40,8 +40,7 @@ cc.Touch = cc.Class.extend(/** @lends cc.Touch# */{
     _startPoint:null,
 
     ctor:function (x, y, id) {
-        this._point = cc.p(x || 0, y || 0);
-        this._id = id || 0;
+        this.setTouchInfo(id, x, y);
     },
 
     /**
