63b0dd256efbb63ecbc53cb19f9e7f053c156844
pandamicro <lphuabin@gmail.com>
2016-06-30 16:15:15 +0800

    Fix IMEDispatcher issue on Chrome

--- cocos2d/text-input/CCIMEDispatcher.js
+++ cocos2d/text-input/CCIMEDispatcher.js
@@ -391,7 +391,7 @@ cc.IMEDispatcher = cc.Class.extend(/**  @lends cc.imeDispatcher# */{
             var tipMessage = delegate.getTipMessage ? delegate.getTipMessage() : "please enter your word:";
             // wechat cover the prompt function .So need use the Window.prototype.prompt
             var userInput;
-            if(window.Window && Window.prototype.prompt != prompt){
+            if(window.Window && Window.prototype.prompt && Window.prototype.prompt != prompt){
                 userInput = Window.prototype.prompt.call(window, tipMessage, this._currentInputString);
             }else{
                 userInput = prompt(tipMessage, this._currentInputString);
