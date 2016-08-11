872a2ea1a415645e8773cdf0e60df1aa9ffedabe
pandamicro <lphuabin@gmail.com>
2016-08-11 14:02:14 +0800

    Fix status sync issue during visit

--- cocos2d/core/base-nodes/CCNodeCanvasRenderCmd.js
+++ cocos2d/core/base-nodes/CCNodeCanvasRenderCmd.js
@@ -465,11 +465,9 @@ cc.Node.RenderCmd.prototype = {
         if (colorDirty || opacityDirty)
             this._updateColor();
 
-        if (locFlag & flags.transformDirty) {
+        if (locFlag & flags.transformDirty)
             //update the transform
             this.transform(parentCmd);
-            this._dirtyFlag = this._dirtyFlag & flags.transformDirty ^ this._dirtyFlag;
-        }
 
         if (locFlag & flags.orderDirty)
             this._dirtyFlag = this._dirtyFlag & flags.orderDirty ^ this._dirtyFlag;
