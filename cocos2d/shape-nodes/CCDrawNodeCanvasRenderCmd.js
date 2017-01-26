9613342cfdc97c45bfa1ca17f1376c138ed8b0fa
giuseppelt <giuseppelt@users.noreply.github.com>
2017-01-26 12:12:27 +0100

    Fix DrawNode canvas displayedOpacity rendering

--- cocos2d/shape-nodes/CCDrawNodeCanvasRenderCmd.js
+++ cocos2d/shape-nodes/CCDrawNodeCanvasRenderCmd.js
@@ -44,7 +44,7 @@
     cc.extend(cc.DrawNode.CanvasRenderCmd.prototype, {
         rendering: function (ctx, scaleX, scaleY) {
             var wrapper = ctx || cc._renderContext, context = wrapper.getContext(), node = this._node;
-            var alpha = node._displayedOpacity / 255;
+            var alpha = this._displayedOpacity / 255;
             if (alpha === 0)
                 return;
 
